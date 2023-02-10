83e937c1181dd3c275e65410933100d9d5e13613
MasWay <70774862+mymasway@users.noreply.github.com>
2023-02-10 07:14:08 +0700

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,3 +1,3 @@
-#& Testing 02 Lifetime 167.172.78.104
-#& Testing 01 Lifetime 157.245.154.58
-#& Testing 03 Lifetime 157.245.154.66
+#& Testing02 Lifetime 167.172.78.104
+#& Testing01 Lifetime 157.245.154.58
+#& Testing03 Lifetime 157.245.154.66
